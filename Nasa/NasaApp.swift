301408113a3acd6c43d13b9b26143ec98bfc8119
steve williams <sjwilliams51@gmail.com>
2022-07-17 17:11:43 -0400

    Initial Commit

---
+++ Nasa/NasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaApp.swift
+//  Nasa
+//
+//  Created by Steven Williams on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct NasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
